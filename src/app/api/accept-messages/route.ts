5d351bc7a942672f6c8e467adcc23a5b84ed1678
architak47 <architawasthi01@gmail.com>
2024-08-15 14:52:16 +0530

    get route for user messages acceptance

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -15,6 +15,9 @@ export async function POST(request: Request){
             {
                 success: false,
                 message: "Not Authenticated"
+            },
+            {
+                status: 401,
             }
         )
     }
@@ -55,6 +58,64 @@ export async function POST(request: Request){
             {
                 success: false,
                 message: "failed to update user status to accept messages"
+            },
+            {
+                status: 500
+            }
+        )
+    }
+}
+
+
+export async function GET(request:Request) {
+    await dbConnect()
+
+    const session = await getServerSession(authOptions)
+    const user: User = session?.user as User
+
+    if(!session || !session.user){
+        return Response.json(
+            {
+                success: false,
+                message: "Not Authenticated"
+            },
+            {
+                status: 401,
+            }
+        )
+    }
+
+    const userId = user._id;
+
+    try {
+        const foundUser = await UserModel.findById(userId);
+        if(!foundUser){
+            return Response.json(
+                {
+                    success: false,
+                    message: "User not found"
+                },
+                {
+                    status: 404
+                }
+            )
+        }
+    
+        return Response.json(
+            {
+                success: true,
+                isAcceptingMessages: foundUser.isAcceptingMessage
+            },
+            {
+                status: 200
+            }
+        )
+    } catch (error) {
+        console.log("failed to update user status to accept messages")
+        return Response.json(
+            {
+                success: false,
+                message: "Error in fetching user status to accept messages"
             },
             {
                 status: 500
